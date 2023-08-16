6258f60bc8a2bbaf7d08c8cbb0dcd2524c779799
Clark Vincent D. Enriquez <vince.cody@gmail.com>
2023-08-16 18:28:31 +0800

    initial commit

---
+++ app/Models/Attendee.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attendee extends Model
+{
+    use HasFactory;
+
+    
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+
+
+use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +21,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('events', EventController::class);
+Route::apiResource('events.attendees', AttendeeController::class)
+    ->scoped(['attendee' => 'event']);
+
